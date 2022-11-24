87453748ef02c3a0fc3dbe09516df82b281c3da8
sanchitadevi <thesanchitadevi@gmail.com>
2022-11-24 21:14:08 +0600

    user created

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ async function run() {
     try {
         const appointmentOptionCollection = client.db('doctorsPortal').collection('appointmentOptions');
         const bookingsCollection = client.db('doctorsPortal').collection('bookings');
+        const usersCollection = client.db('doctorsPortal').collection('users');
 
         app.get('/appointmentOptions', async (req, res) => {
             const date = req.query.date;
@@ -60,7 +61,7 @@ async function run() {
                                 }
                             ],
                             as: 'booked'
-                        },
+                        }
                     },
                     {
                         $project: {
@@ -97,11 +98,39 @@ async function run() {
         app.delete('/bookings/:id)
         */
 
+
+        /* bookings */
+        app.get('/bookings', async (req, res) => {
+            const email = req.query.email;
+            const query = { email: email };
+            const bookings = await bookingsCollection.find(query).toArray();
+            res.send(bookings);
+        })
+
         app.post('/bookings', async (req, res) => {
             const booking = req.body;
             console.log(booking);
 
+            const query = {
+                appointmentDate: booking.appointmentDate,
+            }
+
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `Already booked on ${booking.appointmentDate}`;
+                return res.send({ acknowledged: false, message });
+            }
+
             const result = await bookingsCollection.insertOne(booking);
+            res.send(result);
+        });
+
+        /* users */
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            const result = await usersCollection.insertOne(user);
             res.send(result);
         })
     }
